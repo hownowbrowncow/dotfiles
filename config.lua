3a8268edef768d64da2495cc1d6e8fb9446cbccb
Nick Coffey <nick@Nick.localdomain>
2024-07-20 19:14:16 -0400

    feat: Add modern configs for: tmux, lvim, git

---
+++ config.lua
@@ -0,0 +1,116 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Example configs: https://github.com/LunarVim/starter.lvim
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+--
+--
+--
+
+lvim.log.level = "info"
+lvim.format_on_save = {
+  enabled = true,
+  timeout = 1000,
+}
+lvim.leader = ","
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+lvim.builtin.telescope.pickers.find_files.hidden = true
+lvim.builtin.treesitter.auto_install = true
+
+lvim.plugins = {
+  { "ishan9299/nvim-solarized-lua" },
+  { "gpanders/editorconfig.nvim" },
+}
+
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  {
+    command = "stylelint",
+    filetypes = {
+      "scss",
+      "less",
+      "css",
+      "sass"
+    },
+    args = { "--fix", "--stdin" }
+  },
+  {
+    command = "eslint_d",
+    filetypes = {
+      "javascriptreact",
+      "javascript",
+      "typescriptreact",
+      "typescript",
+    },
+  },
+  {
+    command = "prettier",
+    filetypes = {
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+      "css",
+      "html",
+      "json",
+      "yaml",
+      "less",
+      "markdown",
+      "graphql"
+    },
+  },
+}
+
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  {
+    command = "eslint_d",
+    filetypes = {
+      "javascriptreact",
+      "javascript",
+      "typescriptreact",
+      "typescript",
+    },
+  },
+}
+
+local code_actions = require "lvim.lsp.null-ls.code_actions"
+code_actions.setup {
+  {
+    command = "eslint_d",
+    args = { "-f" },
+    filetypes = {
+      "javascriptreact",
+      "javascript",
+      "typescriptreact",
+      "typescript",
+    },
+  },
+}
+
+require("lvim.lsp.manager").setup("cssls", {
+  settings = {
+    css = {
+      validate = true,
+      lint = {
+        unknownAtRules = "ignore"
+      }
+    },
+    scss = {
+      validate = true,
+      lint = {
+        unknownAtRules = "ignore"
+      }
+    },
+    less = {
+      validate = true,
+      lint = {
+        unknownAtRules = "ignore"
+      }
+    },
+  },
+})
